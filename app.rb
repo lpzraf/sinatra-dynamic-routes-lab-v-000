d7caf37da70e2de631767c3ae51a7fd34a2a6c43
Rafael Lopez <lpz.raf@gmail.com>
2018-12-27 18:43:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,6 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do
-    @oper =
     if params[:operation] == "add"
       params[:number1].to_i + params[:number2].to_i
     elsif params[:operation] == "subtract"
